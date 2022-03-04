96518ae8725f112451ccf7fa7ae486fc15665cc8
ltrucco <56170216+ltrucco@users.noreply.github.com>
2022-03-04 18:43:17 -0300

    deploymet

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
 import 'boxicons/css/boxicons.min.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, HashRouter} from 'react-router-dom';
 import AppLayout from './components/layout/AppLayout';
 import FriendsPage from './pages/friendsPage/FriendsPage';
 
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
     
       <Routes>
-        <Route path='/modakFront' element={<AppLayout/>}>
+        <Route path='/' element={<AppLayout/>}>
           <Route index element={<FriendsPage/>} />
-          <Route path='/modakFront/users' element={<FriendsPage/>} />
+          <Route path='/users' element={<FriendsPage/>} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
